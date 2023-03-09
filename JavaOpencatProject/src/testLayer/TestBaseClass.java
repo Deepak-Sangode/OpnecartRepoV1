26bb9965e5f0bae0ef05399ac8d269659fba72af
Deepak291089 <dpsangode29@gmail.com>
2023-03-09 11:55:10 +0530

    first commit

---
+++ JavaOpencatProject/src/testLayer/TestBaseClass.java
@@ -0,0 +1,12 @@
+package testLayer;
+
+public class TestBaseClass {
+
+	public static void main(String[] args) {
+		System.out.println("TestBase Class");
+		System.out.println("POM.xml");
+		System.out.println("Util");
+
+	}
+
+}
